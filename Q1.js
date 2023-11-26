49748d9ce19e274816fe572d068d00bb6bff7a14
AsadAbbas <assadgamerchannel1@gmail.com>
2023-11-26 04:10:43 -0800

    first commit

---
+++ Q1.js
@@ -0,0 +1,2 @@
+let assad = console.log("Hello Assad, would you like to learn some python");
+export {};
---
+++ Q1.ts
@@ -0,0 +1,1 @@
+let assad = console.log("Hello Assad, would you like to learn some python");
